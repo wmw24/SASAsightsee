4514e231103d56b5c396b45c0aa18b3bf72e39e8
davide <d@vide.bz>
2014-05-10 19:40:28 +0200

    async

--- src/wmw24/sasasightsee/client/SASAsightsee.java
+++ src/wmw24/sasasightsee/client/SASAsightsee.java
@@ -1,7 +1,7 @@
+
 package wmw24.sasasightsee.client;
 
 import java.util.ArrayList;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.jsonp.client.JsonpRequestBuilder;
@@ -14,51 +14,57 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public class SASAsightsee implements EntryPoint
 {
 
-	private ArrayList<Poi> poilist = new ArrayList<Poi>();
+   @Override
+   public void onModuleLoad()
+   {
+      Window.alert("Hello from gwt");
+      this.osmRequest();
+   }
 
-	@Override
-	public void onModuleLoad()
-	{
-		Window.alert("Hello from gwt");
+   private void osmRequest()
+   {
+      String url = "http://overpass-api.de/api/interpreter?data=[out:json];%28node[amenity~%22bar|restaurant|cafe|hospital%22]%2846.46,11.28,46.51,11.35%29;way[amenity~%22bar|restaurant|cafe|hospital%22]%2846.46,11.28,46.51,11.35%29;node[amenity~%22bar|restaurant|cafe|hospital%22]%2846.65,11.13,46.68,11.18%29;way[amenity~%22bar|restaurant|cafe|hospital%22]%2846.65,11.13,46.68,11.18%29;%3E%29;out;";
 
-	}
+      JsonpRequestBuilder jsonp = new JsonpRequestBuilder();
+      jsonp.setCallbackParam("jsonp");
 
-	private void osmRequest()
-	{
-		String url = "http://overpass-api.de/api/interpreter?data=[out:json];%28node[amenity~%22bar|restaurant|cafe|hospital%22]%2846.46,11.28,46.51,11.35%29;way[amenity~%22bar|restaurant|cafe|hospital%22]%2846.46,11.28,46.51,11.35%29;node[amenity~%22bar|restaurant|cafe|hospital%22]%2846.65,11.13,46.68,11.18%29;way[amenity~%22bar|restaurant|cafe|hospital%22]%2846.65,11.13,46.68,11.18%29;%3E%29;out;";
+      jsonp.requestObject(url, new AsyncCallback<OSMResponse>()
+      {
 
-		JsonpRequestBuilder jsonp = new JsonpRequestBuilder();
-		jsonp.setCallbackParam("jsonp");
+         @Override
+         public void onSuccess(OSMResponse response)
+         {
+            ArrayList<Poi> poilist = new ArrayList<Poi>();
 
-		jsonp.requestObject(url, new AsyncCallback<OSMResponse>()
-		{
+            JsArray<OSMObject> elements = response.getElements();
+            Window.alert(elements.length() + " Object read from OSM");
+            for (int i = 0; i < elements.length(); i++)
+            {
+               OSMObject object = elements.get(i);
+               if (object.getType() == "node" && object.getTags() != null)
+               {
+                  Poi poi = new Poi();
+                  poi.setLat(object.getLat());
+                  poi.setLon(object.getLon());
+                  poi.setName(object.getTags().getName());
+                  poi.setAmenity(object.getTags().getAmenity());
+                  poilist.add(poi);
+               }
+            }
+            SASAsightsee.this.onOSMReady(poilist);
+         }
 
-			@Override
-			public void onSuccess(OSMResponse response)
-			{
-				JsArray<OSMObject> elements = response.getElements();
-				Window.alert(elements.length() + " Object read from OSM");
-				for (int i = 0; i < elements.length(); i++)
-				{
-					OSMObject object = elements.get(i);
-					if (object.getType() == "node" && object.getTags() != null)
-					{
-						Poi poi = new Poi();
-						poi.setLat(object.getLat());
-						poi.setLon(object.getLon());
-						poi.setName(object.getTags().getName());
-						poi.setAmenity(object.getTags().getAmenity());
-						poilist.add(poi);
-					}
-				}
-			}
+         @Override
+         public void onFailure(Throwable caught)
+         {
+            // TODO Auto-generated method stub
+            return;
+         }
+      });
+   }
 
-			@Override
-			public void onFailure(Throwable caught)
-			{
-				// TODO Auto-generated method stub
-				return;
-			}
-		});
-	}
+   private void onOSMReady(ArrayList<Poi> poilist)
+   {
+
+   }
 }
