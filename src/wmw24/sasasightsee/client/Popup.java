ab9e8aa2631c73682069b033412a19e93df6721d
markuswindegger <markus@mowiso.com>
2014-05-11 05:19:46 +0200

    added icon options to popup

--- src/wmw24/sasasightsee/client/Popup.java
+++ src/wmw24/sasasightsee/client/Popup.java
@@ -1,4 +1,3 @@
-
 package wmw24.sasasightsee.client;
 
 import it.bz.tis.sasabus.backend.shared.SASAbusDBDataReady;
@@ -10,6 +9,7 @@ import java.util.Date;
 import java.util.Map;
 
 import bz.davide.dmweb.client.leaflet.DistanceCalculator;
+import bz.davide.dmweb.client.leaflet.IconOptions;
 import bz.davide.dmweb.shared.view.ButtonView;
 import bz.davide.dmweb.shared.view.DMClickEvent;
 import bz.davide.dmweb.shared.view.DMClickHandler;
@@ -28,172 +28,181 @@ import com.google.gwt.user.datepicker.client.CalendarUtil;
 
 public class Popup extends DivView
 {
-   ButtonView            buttonView;
-   ArrayList<BusStation> busStations;
+	ButtonView buttonView;
+	ArrayList<BusStation> busStations;
 
-   BusStation            nearestToPoi;
+	BusStation nearestToPoi;
 
-   public Popup(Poi poi, ArrayList<BusStation> busStations, Map<String, Weather> weather,
-		   Date currDate)
-   {
-      this.busStations = busStations;
-      this.setStyleName("popup");
-      this.appendChild(new SpanView(poi.getName()));
+	IconOptions iconOptions;
 
-      String today = DateTimeFormat.getFormat("yyyy-MM-dd").format(currDate);
-      Date date = (Date)currDate.clone();
-      CalendarUtil.addDaysToDate(date, 1);
-      String tomorrow = DateTimeFormat.getFormat("yyyy-MM-dd").format(date);      
-      
-      DivView weatherDiv = new DivView("weather");
+	public Popup(Poi poi, ArrayList<BusStation> busStations,
+			Map<String, Weather> weather, Date currDate, IconOptions iconOption)
+	{
+		this.busStations = busStations;
+		this.setStyleName("popup");
+		this.appendChild(new SpanView(poi.getName()));
+		this.iconOptions = iconOption;
 
-      DivView todayWeather = new DivView("today");
-      
-      ImgView imageView = new ImgView(weather.get(today).getImageURL("2"));
-      SpanView spanView = new SpanView(weather.get(today).getDescription("2")
-    		  + " - " + weather.get(today).getTempMin("2") + "-"
-    		  + weather.get(today).getTempMax("2") + "°");
-      
-      todayWeather.appendChild(imageView);
-      todayWeather.appendChild(spanView);
+		String today = DateTimeFormat.getFormat("yyyy-MM-dd").format(currDate);
+		Date date = (Date) currDate.clone();
+		CalendarUtil.addDaysToDate(date, 1);
+		String tomorrow = DateTimeFormat.getFormat("yyyy-MM-dd").format(date);
 
-      weatherDiv.appendChild(todayWeather);
+		DivView weatherDiv = new DivView("weather");
 
-      DivView tomorrowWeather = new DivView("tomorrow");
+		DivView todayWeather = new DivView("today");
 
-      imageView = new ImgView(weather.get(tomorrow).getImageURL("2"));
-      spanView = new SpanView(weather.get(tomorrow).getDescription("2")
-    		  + " - " + weather.get(tomorrow).getTempMin("2") + "-"
-    		  + weather.get(tomorrow).getTempMax("2") + "°");
+		ImgView imageView = new ImgView(weather.get(today).getImageURL("2"));
+		SpanView spanView = new SpanView(weather.get(today).getDescription("2")
+				+ " - " + weather.get(today).getTempMin("2") + "-"
+				+ weather.get(today).getTempMax("2") + "°");
 
-      tomorrowWeather.appendChild(imageView);
-      tomorrowWeather.appendChild(spanView);
+		todayWeather.appendChild(imageView);
+		todayWeather.appendChild(spanView);
 
-      weatherDiv.appendChild(tomorrowWeather);
+		weatherDiv.appendChild(todayWeather);
 
-      this.appendChild(weatherDiv);
+		DivView tomorrowWeather = new DivView("tomorrow");
 
+		imageView = new ImgView(weather.get(tomorrow).getImageURL("2"));
+		spanView = new SpanView(weather.get(tomorrow).getDescription("2")
+				+ " - " + weather.get(tomorrow).getTempMin("2") + "-"
+				+ weather.get(tomorrow).getTempMax("2") + "°");
 
-      this.buttonView = new ButtonView("With bus here");
-      this.appendChild(this.buttonView);
-      this.buttonView.addClickHandler(new DMClickHandler()
-      {
+		tomorrowWeather.appendChild(imageView);
+		tomorrowWeather.appendChild(spanView);
 
-         @Override
-         public void onClick(DMClickEvent event)
-         {
-            Popup.this.onButtonClick();
-         }
+		weatherDiv.appendChild(tomorrowWeather);
 
-      });
+		this.appendChild(weatherDiv);
 
+		this.buttonView = new ButtonView("With bus here");
+		this.appendChild(this.buttonView);
+		this.buttonView.addClickHandler(new DMClickHandler()
+		{
 
-      this.nearestToPoi = this.nearest(poi.getLat(), poi.getLon());
+			@Override
+			public void onClick(DMClickEvent event)
+			{
+				Popup.this.onButtonClick();
+			}
 
-   }
+		});
 
-   private void onButtonClick()
-   {
-      this.appendChild(new SpanView("Nearest busStation: " + this.nearestToPoi.getName()));
+		this.nearestToPoi = this.nearest(poi.getLat(), poi.getLon());
 
-      this.buttonView.setLabel("Searching your position ...");
-      PositionOptions positionOptions = new PositionOptions();
-      positionOptions.setHighAccuracyEnabled(true);
-      positionOptions.setTimeout(10000);
+	}
 
-      // http://openlayers.org/dev/examples/geolocation.html
+	private void onButtonClick()
+	{
+		this.appendChild(new SpanView("Nearest busStation: "
+				+ this.nearestToPoi.getName()));
 
-      Geolocation geoloc = Geolocation.getIfSupported();
-      if (geoloc != null)
-      {
-         geoloc.getCurrentPosition(new Callback<Position, PositionError>()
-         {
-            @Override
-            public void onSuccess(Position result)
-            {
+		this.buttonView.setLabel("Searching your position ...");
+		PositionOptions positionOptions = new PositionOptions();
+		positionOptions.setHighAccuracyEnabled(true);
+		positionOptions.setTimeout(10000);
 
-               double lon = result.getCoordinates().getLongitude();
-               double lat = result.getCoordinates().getLatitude();
-               double accuracy = result.getCoordinates().getAccuracy();
+		// http://openlayers.org/dev/examples/geolocation.html
 
-               BusStation nearestToYou = Popup.this.nearest(lat, lon);
-               Popup.this.appendChild(new SpanView("Nearest busStation: " + nearestToYou.getName()));
+		Geolocation geoloc = Geolocation.getIfSupported();
+		if (geoloc != null)
+		{
+			geoloc.getCurrentPosition(new Callback<Position, PositionError>()
+			{
+				@Override
+				public void onSuccess(Position result)
+				{
 
-               try
-               {
-                  SASAbusDBClientImpl.singleton.calcRoute(nearestToYou.getId(), Popup.this.nearestToPoi.getId(), 201405090900L, new SASAbusDBDataReady<ConRes>()
-                  {
+					double lon = result.getCoordinates().getLongitude();
+					double lat = result.getCoordinates().getLatitude();
+					double accuracy = result.getCoordinates().getAccuracy();
 
-                     @Override
-                     public void ready(ConRes data)
-                     {
-                        Popup.this.displayRoute(data);
-                     }
-                  });
-               }
-               catch (Exception e)
-               {
-                  // TODO Auto-generated catch block
-                  e.printStackTrace();
-               }
+					BusStation nearestToYou = Popup.this.nearest(lat, lon);
+					Popup.this.appendChild(new SpanView("Nearest busStation: "
+							+ nearestToYou.getName()));
 
-               //Window.alert("lat: " + lat + " lon: " + lon + " acc (meter): " + accuracy);
+					try
+					{
+						SASAbusDBClientImpl.singleton.calcRoute(
+								nearestToYou.getId(),
+								Popup.this.nearestToPoi.getId(), 201405090900L,
+								new SASAbusDBDataReady<ConRes>()
+								{
 
-               //LatLng position = new LatLng(lat, lon);
-               //SASAbusMapAttachHandler.this.map.leafletMap.setView(position, 16);
-            }
+									@Override
+									public void ready(ConRes data)
+									{
+										Popup.this.displayRoute(data);
+									}
+								});
+					}
+					catch (Exception e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 
-            @Override
-            public void onFailure(PositionError reason)
-            {
-               Window.alert("Failure: " + reason.getMessage());
-            }
-         },
-                                   positionOptions);
-      }
-      else
-      {
-         Window.alert("Your browser does not support localization");
-      }
-   }
+					// Window.alert("lat: " + lat + " lon: " + lon +
+					// " acc (meter): " + accuracy);
 
-   void displayRoute(ConRes conRes)
-   {
-      this.appendChild(new RouteDetail(conRes));
-   }
+					// LatLng position = new LatLng(lat, lon);
+					// SASAbusMapAttachHandler.this.map.leafletMap.setView(position,
+					// 16);
+				}
 
-   BusStation nearest(double lat, double lon)
-   {
-      BusStation best = this.busStations.get(0);
-      for (int i = 1; i < this.busStations.size(); i++)
-      {
-         BusStation busStation = this.busStations.get(i);
+				@Override
+				public void onFailure(PositionError reason)
+				{
+					Window.alert("Failure: " + reason.getMessage());
+				}
+			}, positionOptions);
+		}
+		else
+		{
+			Window.alert("Your browser does not support localization");
+		}
+	}
 
-         double lat1 = best.getBusStops().get(0).getLat();
-         double lon1 = best.getBusStops().get(0).getLon();
+	void displayRoute(ConRes conRes)
+	{
+		this.appendChild(new RouteDetail(conRes));
+	}
 
-         double lat2 = busStation.getBusStops().get(0).getLat();
-         double lon2 = busStation.getBusStops().get(0).getLon();
+	BusStation nearest(double lat, double lon)
+	{
+		BusStation best = this.busStations.get(0);
+		for (int i = 1; i < this.busStations.size(); i++)
+		{
+			BusStation busStation = this.busStations.get(i);
 
-         double distance1 = DistanceCalculator.distanceMeter(lat1, lon1, lat, lon);
-         double distance2 = DistanceCalculator.distanceMeter(lat2, lon2, lat, lon);
+			double lat1 = best.getBusStops().get(0).getLat();
+			double lon1 = best.getBusStops().get(0).getLon();
 
-         if (distance2 < distance1)
-         {
-            best = busStation;
-         }
-      }
-      return best;
-   }
+			double lat2 = busStation.getBusStops().get(0).getLat();
+			double lon2 = busStation.getBusStops().get(0).getLon();
 
-   static String formatTime(String time)
-   {
-      if (time.startsWith("00d"))
-      {
-         time = time.substring(3);
-      }
-      String[] timeParts = time.split(":");
-      time = timeParts[0] + ":" + timeParts[1];
-      return time;
-   }
+			double distance1 = DistanceCalculator.distanceMeter(lat1, lon1,
+					lat, lon);
+			double distance2 = DistanceCalculator.distanceMeter(lat2, lon2,
+					lat, lon);
+
+			if (distance2 < distance1)
+			{
+				best = busStation;
+			}
+		}
+		return best;
+	}
+
+	static String formatTime(String time)
+	{
+		if (time.startsWith("00d"))
+		{
+			time = time.substring(3);
+		}
+		String[] timeParts = time.split(":");
+		time = timeParts[0] + ":" + timeParts[1];
+		return time;
+	}
 }
