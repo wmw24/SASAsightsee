9f9a50af2a245dcfc27fa360d45838e507d9c583
markuswindegger <markus@mowiso.com>
2014-05-11 10:08:05 +0200

    added twitter timeline

--- src/wmw24/sasasightsee/client/DetailOverlay.java
+++ src/wmw24/sasasightsee/client/DetailOverlay.java
@@ -141,6 +141,10 @@ public class DetailOverlay extends DivView
 
 		this.nearestToPoi = this.nearest(poi.getLat(), poi.getLon());
 
+		HTML html = new HTML(
+				"<a class=\"twitter-timeline\"  href=\"https://twitter.com/search?q=%23angelhackrules\"  data-widget-id=\"465401704247074816\">Tweets über \"#angelhackrules\"</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+\"://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>");
+		this.getElement().appendChild(html.getElement());
+
 		if (poi.getAttr("wikipedia") != null)
 		{
 			this.wikireq = new WikipediaTextRequest(poi.getAttr("wikipedia"),
